0965e87420f3aac239385ce3dd8fcc4cfb89bb59
jerem <jerem@DESKTOP-B6M3VPO>
2021-03-11 19:00:26 +0100

    proba

---
+++ PanelZoom/LoginWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Flurl.Http;
+
+namespace PanelZoom
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        string GetHashString (byte[] hash)
+        {
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sBuilder.Append(hash[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+
+        private void Loggin_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                // krzychu_zoom KuZooM1914KRz
+                string Password;
+                string Username;
+                string UserPlusPass;
+                string token;
+
+                Password = PasswordBox.Password;
+                Username = LoginTextBox.Text;
+                Username = Username.ToLower();
+
+                UserPlusPass = Username + Password;
+
+                SHA256 sHA256 = SHA256.Create();
+                byte[] hash = sHA256.ComputeHash(Encoding.UTF8.GetBytes(UserPlusPass));
+
+                token = "token=" + GetHashString(hash);
+
+                WebRequest webRequest = WebRequest.Create("https://panel-rm.yum.pl/API/get_contacts_list.php");
+
+                webRequest.ContentType = "application/x-www-form-urlencoded; charset=utf-8";
+                webRequest.Method = "POST";
+
+                StreamWriter streamWriter = new StreamWriter(webRequest.GetRequestStream());
+
+                streamWriter.Write(token);
+                streamWriter.Flush();
+
+                var httpResponse = (HttpWebResponse)webRequest.GetResponse();
+
+                StreamReader streamReader = new StreamReader(httpResponse.GetResponseStream());
+
+                string result = streamReader.ReadToEnd();
+
+                label.Content = result + UserPlusPass + " " + token;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ PanelZoom/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
+namespace PanelZoom
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {        
+            InitializeComponent();
+        }
+
+        private void MainWindow_Initialized(object sender, EventArgs e)
+        {
+            string connectionString = "server = mysql-727668.vipserv.org; user = rafik73_mapy; database = rafik73_mapy; password = Rafaello73";
+
+            MySqlConnection mySqlConnection = new MySqlConnection(connectionString);
+
+            try
+            {
+                mySqlConnection.Open();
+
+                string ContactName;
+                string zoom_password;
+                string zoom_id;
+                string sql = "SELECT * FROM panelrm_contacts";
+
+                bool special;
+
+                int contact_group;
+                
+                MySqlCommand cmd = new MySqlCommand(sql, mySqlConnection);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    ContactName = rdr.GetString("contact_name");
+                    special = rdr.GetBoolean("special");
+                    contact_group = rdr.GetInt32("contact_group");
+                    zoom_password = rdr.GetString("zoomlink_pass");
+                    zoom_id = rdr.GetString("zoomlink_id");
+                    
+                    Creating_Front(ContactName, special, contact_group, zoom_password, zoom_id);
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        void Creating_Front(string ContactName, bool special, int contact_group, string Password, string ID)
+        {
+            Button button = new Button();
+
+            if(special == false)
+            {
+                button.Background = Brushes.White;
+            }
+
+            else if(special == true)
+            {
+                button.Background = Brushes.Gray;
+                button.Foreground = Brushes.White;
+            }
+
+            if(contact_group == 0 && special == false)
+            {
+                button.Foreground = Brushes.Blue;
+            }
+
+            button.Content = ContactName;
+
+            button.Click += (s, e) => 
+            {
+                string link;
+                link = "zoommtg://zoom.us/join?confno=" + ID + "&pwd=" + Password;
+
+                Process.Start(link);
+            };
+
+            stackpanel.Children.Add(button);
+            
+        }
+    }
+}
